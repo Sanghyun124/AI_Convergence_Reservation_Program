78c8f2ea89bce531bd3248884ece20e52c369135
C-H-Kim <nick6253@naver.com>
2023-08-30 03:45:55 +0900

    Environment Configuration

---
+++ src/main/java/com/example/ReservationSystem/ReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservationSystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
