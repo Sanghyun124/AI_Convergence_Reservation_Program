6955095af0b149e93d03a5b3484f65c66a2528cb
Sanghyun124 <cloush24@gmail.com>
2023-09-04 19:21:41 +0900

    feat : Add Reservation Table
    Set MySQL and add reservation table

---
+++ backend/reservation/src/main/java/CSS/ReservationSystem/domain/Reservation.java
@@ -0,0 +1,26 @@
+package CSS.ReservationSystem.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime EndTime;
+}
+
