21cf998ce26d2655ff7140c20333446a5beaf8d4
UnbroKen <unbroken2650@gmail.com>
2023-11-06 17:12:24 -0500

    Revert "Initial commit"

--- frontend/reservation_system/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './Router';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
